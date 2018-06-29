98d1586a63561533fce47ab6d732db5d53f6428c
Marcos Leal <marklegend@live.com>
2018-06-29 16:15:12 -0300

    fix string ref

--- src/carousel.js
+++ src/carousel.js
@@ -173,18 +173,18 @@ const Carousel = createReactClass({
     return (
       <div
         className={['slider', this.props.className || ''].join(' ')}
-        ref="slider"
+        ref={el => this.slider = el}
         style={assign(this.getSliderStyles(), this.props.style || {})}
       >
         <div
           className="slider-frame"
-          ref="frame"
+          ref={el => this.frame = el}
           style={this.getFrameStyles()}
           {...this.getTouchEvents()}
           {...this.getMouseEvents()}
           onClick={this.handleClick}
         >
-          <ul className="slider-list" ref="list" style={this.getListStyles()}>
+          <ul className="slider-list" ref={el => this.list = el} style={this.getListStyles()}>
             {children}
           </ul>
         </div>
@@ -775,7 +775,7 @@ const Carousel = createReactClass({
       slideHeight;
 
     slidesToScroll = props.slidesToScroll;
-    frame = this.refs.frame;
+    frame = this.frame;
     firstSlide = frame.childNodes[0].childNodes[0];
     if (firstSlide) {
       firstSlide.style.height = 'auto';
@@ -1068,7 +1068,7 @@ Carousel.ControllerMixin = {
   },
   setCarouselData(carousel) {
     var data = this.state.carousels;
-    data[carousel] = this.refs[carousel];
+    data[carousel] = this[carousel];
     this.setState({
       carousels: data,
     });
